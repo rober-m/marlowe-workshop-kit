739de14706914101c7a667fcf7cca5dc6c0f0132
Luka Kurnjek <luka.kurnjek@iohk.io>
2023-10-24 13:45:03 +0200

    Updating generate_seeds_..1 script to generate base type addresses.

--- creating_wallets/generate_seeds_and_addresses_1.sh
+++ creating_wallets/generate_seeds_and_addresses_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script that generates wallet seeds and addresses with the cardano-address tool 
+# Script that generates wallet seeds and addresses of type base with the cardano-address tool 
 
 set -e
 number_of_wallets=$1
@@ -13,16 +13,27 @@ fi
 
 mkdir "wallet_files"
 
-# Loop for generating seeds and addresses 
+# Loop for generating seeds and addresses of type base that contain a staking part 
 for i in $(seq 1 $number_of_wallets)
 do
     seed=$(cardano-address recovery-phrase generate --size 15) 
     echo $seed > "wallet_files/phrase_$i.prv"
     echo $seed >> "seeds.txt"
-    cardano-address key from-recovery-phrase Shelley < "wallet_files/phrase_$i.prv" > "wallet_files/root_$i.xsk"
-    cardano-address key child 1852H/1815H/0H/0/0 < "wallet_files/root_$i.xsk" | cardano-address key public --with-chain-code > "wallet_files/addr_$i.xvk"
-    address=$(cardano-address address payment --network-tag testnet < "wallet_files/addr_$i.xvk") 
-    echo $address > "wallet_files/payment_$i.addr"
-    echo $address >> "addresses.txt" 
+
+    rootPK=$(echo $seed | cardano-address key from-recovery-phrase Shelley)
+    echo $rootPK > "wallet_files/root_$i.xsk" 
+
+    extendedSVK=$(echo $rootPK | cardano-address key child 1852H/1815H/0H/2/0 | cardano-address key public --with-chain-code)
+    echo $extendedSVK > "wallet_files/stake_$i.xvk"
+
+    paymentVK=$(echo $rootPK | cardano-address key child 1852H/1815H/0H/0/0 | cardano-address key public --with-chain-code)
+    echo $paymentVK > "wallet_files/addr_$i.xvk"
+
+    paymentADDR=$(echo $paymentVK | cardano-address address payment --network-tag testnet)
+    echo $paymentADDR > "wallet_files/payment_$i.addr"
+
+    delegatedPADDR=$(echo $paymentADDR | cardano-address address delegation $extendedSVK)
+    echo $delegatedPADDR > "wallet_files/base_$i.addr" 
+    echo $delegatedPADDR >> "addresses.txt" 
 done
 
